546ebc8262e762788bdfb616128b04b6f779d259
Matěj Kohútek <matej.kohutek@gmail.com>
2019-02-24 14:27:00 +0100

    Change assebly information - MIT licence and created by me

--- PersonalInfoForm/PersonalInfoForm/Properties/AssemblyInfo.cs
+++ PersonalInfoForm/PersonalInfoForm/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PersonalInfoForm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("School Project - by Matěj Kohútek - xkohm016")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PersonalInfoForm")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("MIT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
